271b696a982af1b23882e7a1ba863301276749d1
jackson <github.com/Consultview/HappyClient>
2018-12-09 08:45:36 -0300

    criando a classe objeto

--- app/src/main/java/consultviewconsultoria/happyclient/object/Object.java
+++ app/src/main/java/consultviewconsultoria/happyclient/object/Object.java
@@ -12,7 +12,60 @@ public class Object implements Serializable{
 
     private long _idVoto;
     private int exelente;
+    private int bom;
+    private int regular;
+    private int ruim;
+    private int pessimo;
 
+    public Object() {
 
+    }
 
+    public long get_idVoto() {
+        return _idVoto;
+    }
+
+    public void set_idVoto(long _idVoto) {
+        this._idVoto = _idVoto;
+    }
+
+    public int getExelente() {
+        return exelente;
+    }
+
+    public void setExelente(int exelente) {
+        this.exelente = exelente;
+    }
+
+    public int getBom() {
+        return bom;
+    }
+
+    public void setBom(int bom) {
+        this.bom = bom;
+    }
+
+    public int getRegular() {
+        return regular;
+    }
+
+    public void setRegular(int regular) {
+        this.regular = regular;
+    }
+
+    public int getRuim() {
+        return ruim;
+    }
+
+    public void setRuim(int ruim) {
+        this.ruim = ruim;
+    }
+
+    public int getPessimo() {
+        return pessimo;
+    }
+
+    public void setPessimo(int pessimo) {
+        this.pessimo = pessimo;
+    }
 }
