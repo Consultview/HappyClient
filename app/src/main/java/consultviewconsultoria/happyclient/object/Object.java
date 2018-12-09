64cd072f3dbe6fec749daadaaf9c46936b961f79
jackson <github.com/Consultview/HappyClient>
2018-12-08 21:57:34 -0300

    criando a classe objeto

--- app/src/main/java/consultviewconsultoria/happyclient/object/Object.java
+++ app/src/main/java/consultviewconsultoria/happyclient/object/Object.java
@@ -1,8 +1,18 @@
 package consultviewconsultoria.happyclient.object;
 
+import java.io.Serializable;
+
 /**
  * Created by janiel on 08/12/2018.
  */
 
-public class Object {
+public class Object implements Serializable{
+
+
+
+    private long _idVoto;
+    private int exelente;
+
+
+
 }
