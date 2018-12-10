bbf249433d47b7619d3ca1382abd1dd18e896114
jackson <github.com/Consultview/HappyClient>
2018-12-10 01:32:12 -0300

    possivel layout criado

--- app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityInicial.java
+++ app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityInicial.java
@@ -1,15 +1,15 @@
 package consultviewconsultoria.happyclient.tela;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.widget.ImageView;
-import android.widget.Toast;
+
 
 import consultviewconsultoria.happyclient.R;
 
@@ -23,19 +23,27 @@ ImageView excelente;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inicial);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
         excelente = findViewById(R.id.imageViewExecelente);
         excelente.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(MainActivityInicial.this, "objeto clicado",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivityInicial.this,MainActivityVoto.class);
+                startActivity(intent);
+                //Toast.makeText(MainActivityInicial.this, "objeto clicado",Toast.LENGTH_SHORT).show();
+
+
+
             }
         });
+
     }
 
 
+
+
+
+    /**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -56,5 +64,5 @@ ImageView excelente;
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
