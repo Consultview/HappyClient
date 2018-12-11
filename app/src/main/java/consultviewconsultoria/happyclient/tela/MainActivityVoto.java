4ac26b19dded5857927b3d850b2d4c0fe6cf8df6
jackson <github.com/Consultview/HappyClient>
2018-12-10 23:59:45 -0300

    criando os clicks

--- app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityVoto.java
+++ app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityVoto.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -38,51 +39,8 @@ public class MainActivityVoto extends AppCompatActivity {
 
     }
 
-    @Override//selection item
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.imageViewExecelente:
-                v.setExelente(+1);
-                Toast.makeText(this, "Inserido com sucesso"+v.getExelente(), LENGTH_LONG).show();
-                break;
 
-            case R.id.imageViewBom:
-                v.setBom(+1);
-                Conection con = new Conection(getApplicationContext());
-                if (v.get_idVoto() == 0) {
-                    con.insertVoto(v);
-                    Toast.makeText(this, "Inserido com sucesso", LENGTH_LONG).show();
-                    con.close();
-                    break;
 
-                } else {
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-                    dlg.setTitle("Aviso !");
-                    dlg.setMessage("Esta sendo testado nao inserido !");
-                    dlg.setNeutralButton("ok", null);
-                    dlg.show();
-                    con.close();
-                    break;
-                }
-
-            case R.id.imageViewRegular:
-                v.setRegular(+1);
-                Toast.makeText(this, "Inserido com sucesso"+v.getRegular(), LENGTH_LONG).show();
-                break;
-
-            case R.id.imageViewRuim:
-                v.setRuim(+1);
-                Toast.makeText(this, "Inserido com sucesso"+v.getRuim(), LENGTH_LONG).show();
-                break;
-
-            case R.id.imageViewPessimo:
-                v.setPessimo(+1);
-                Toast.makeText(this, "Inserido com sucesso"+v.getPessimo(), LENGTH_LONG).show();
-                break;
-        }
-
-        return super.onOptionsItemSelected( item );
-    }
 
 
     public void castById() {
@@ -96,4 +54,51 @@ public class MainActivityVoto extends AppCompatActivity {
     }
 
 
+    public void clickExcelente(View view) {
+
+        v.setExelente(+1);
+        Toast.makeText( this, "esse objeto foi clicado"+v.getExelente(), Toast.LENGTH_SHORT ).show();
+
+    }
+
+    public void clickBom(View view) {
+        v.setBom(+1);
+        Toast.makeText( this, "esse objeto foi clicado"+v.getBom(), Toast.LENGTH_SHORT ).show();
+
+
+    }
+
+    public void clickRegular(View view) {
+
+        v.setRegular(+1);
+        Toast.makeText( this, "esse objeto foi clicado"+v.getRegular(), Toast.LENGTH_SHORT ).show();
+    }
+
+    public void clickRuim(View view) {
+        v.setRuim(+1);
+        Toast.makeText( this, "esse objeto foi clicado"+v.getRuim(), Toast.LENGTH_SHORT ).show();
+    }
+
+    public void clickPessimo(View view) {
+        v.setPessimo(+1);
+        Toast.makeText( this, "esse objeto foi clicado"+v.getPessimo(), Toast.LENGTH_SHORT ).show();
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
