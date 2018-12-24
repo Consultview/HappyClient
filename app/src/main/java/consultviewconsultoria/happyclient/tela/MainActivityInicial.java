0f65f1991786b567d5dbec4d08d9a8038a4130e9
consultview <jackson.lima@brf-br.com>
2018-12-24 00:22:56 -0300

    atualizando !

--- app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityInicial.java
+++ app/src/main/java/consultviewconsultoria/happyclient/tela/MainActivityInicial.java
@@ -1,68 +1,32 @@
 package consultviewconsultoria.happyclient.tela;
 
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.view.View;
-
 import android.widget.ImageView;
-
-
+import android.widget.Toast;
 import consultviewconsultoria.happyclient.R;
 
 public class MainActivityInicial extends AppCompatActivity {
 
-
-ImageView excelente;
-
+    ImageView excelente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inicial);
 
-
         excelente = findViewById(R.id.imageViewExecelente);
         excelente.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivityInicial.this,MainActivityVoto.class);
+                Intent intent = new Intent(MainActivityInicial.this, MainActivityVoto.class);
                 startActivity(intent);
-                //Toast.makeText(MainActivityInicial.this, "objeto clicado",Toast.LENGTH_SHORT).show();
-
-
-
+                Toast.makeText(MainActivityInicial.this, "Bem Vindo !", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
 
-
-
-
-    /**
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity_inicial, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 }
